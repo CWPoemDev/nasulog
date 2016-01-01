43750c456a86b0fef6b3b471e33c7c680d10a411
Kenta Suzuki <ksuzuki180@gmail.com>
2016-01-01 19:04:40 +0900

    fix #22 ユーザー編集画面を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
 
   def login_required
     if session[:user_id]
       @current_user = User.find(session[:user_id])
     end
+  end
+
+  def current_user
+    return @current_user if @current_user
+    @current_user = User.find(session[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    session.delete(:user_id)
+    nil
   end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+# ユーザー情報を編集する
+class UsersController < ApplicationController
+  before_action :login_required
+  before_action :set_user, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to edit_user_path, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
