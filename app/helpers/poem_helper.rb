a26744e2b1ee1db962526dba203fba7eb1c2897a
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-01-03 20:58:28 +0900

    extensionsを外に出す

--- app/helpers/poem_helper.rb
+++ app/helpers/poem_helper.rb
@@ -1,7 +1,8 @@
 module PoemHelper
   def markdown(text)
     renderer = Redcarpet::Render::HTML.new(filter_html: true)
-    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    extensions = {}
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(text).html_safe
   end
 end
