5eee1fa4a67768614477ca0c79f525619fa15794
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-01-18 19:55:32 +0900

    /hone.index => /poems/index を、viewのシンボリックリンクじゃなくて、コントローラ側でリダイレクトをかけるようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @poems = Poem.all.order(id: :desc).includes(:user)
+    respond_to do |format|
+      format.html
+      format.json { render template: "poems/index" }
+    end
   end
 end
