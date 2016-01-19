7aa19904e6086db294a2ef38cb867e93c4eac6de
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-01-19 15:06:09 +0900

    read_poemsのjsonリクエスト時に、CSRF対策を外す

--- app/controllers/read_poems_controller.rb
+++ app/controllers/read_poems_controller.rb
@@ -1,13 +1,27 @@
 class ReadPoemsController < ApplicationController
   before_filter :login_required
+  skip_before_action :verify_authenticity_token, if: :json_request?
 
   def create
     current_user.read_poems.create(poem_id: params[:poem_id])
-    redirect_to :back
+    if json_request?
+      render json: {status: :success}, status: :created
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
     current_user.read_poems.destroy(params[:id])
-    redirect_to :back
+    if json_request?
+      render nothing: true, status: :no_content
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+  def json_request?
+    request.format.json?
   end
 end
