cc2be837dbe13ba17bb8792d394e66857d6afc71
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-01-26 10:39:17 +0900

    非同期「読んだよ」

--- app/assets/javascripts/poem.js
+++ app/assets/javascripts/poem.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+((function(){
+
+  function setCallbackForBtnPost() {
+    $("a#btn_read_poem").on("ajax:success", function(e, data, status, xhr) {
+      $(this).parent().replaceWith($(data).find("a#btn_read_poem").parent());
+      setCallbackForBtnPost();
+    });
+  }
+
+  function setCallbackForBtnRemove() {
+    $("a.btn_read_poem_icon").on("ajax:success", function(e, data, status, xhr) {
+      $(this).remove();
+    });
+  }
+
+  $(function(){
+    setCallbackForBtnPost();
+    setCallbackForBtnRemove();
+  });
+
+})());
