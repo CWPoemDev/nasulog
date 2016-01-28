a97365a07b84f4eb2be69accde0fc6d792c7530d
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-01-29 01:28:46 +0900

    FIX #40 読んだよマークをつけたポエムの一覧を見れるようにする

--- app/controllers/read_poems_controller.rb
+++ app/controllers/read_poems_controller.rb
@@ -1,4 +1,8 @@
 class ReadPoemsController < ApplicationController
+  def index
+    @read_poems = Poem.joins(:read_poems).where("read_poems.user_id = ?", current_user.id).group(:id).order(created_at: :DESC)
+  end
+
   def create
     current_user.read_poems.create(poem_id: params[:poem_id])
     redirect_to :back
