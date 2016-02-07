65562824b6cd3eeffa66997014d7cb3fbbb133ff
kibitan <uzukifirst@gmail.com>
2016-02-07 16:09:26 +0900

    missing merge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
   config.assets.digest = true
   config.assets.raise_runtime_errors = true
 
-<<<<<<< HEAD
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
@@ -37,12 +36,11 @@ Rails.application.configure do
   # 	authentication:        'plain',
   # 	enable_starttls_auto:  true
   # }
-=======
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
->>>>>>> append `rake rails:update`
 end
