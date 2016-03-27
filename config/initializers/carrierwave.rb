1d30d02269cd98155b8d2dad8eb231d8474456aa
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-03-27 14:02:39 +0900

    ひとまずテスト時は回避するようにする

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_ACCESS_KEY'],
-    :region                 => ENV['AWS_REGION']
-  }
+if ENV['AWS_KEY_ID'] && ENV['AWS_ACCESS_KEY'] && ENV['AWS_REGION'] && ENV['AWS_S3_BUCKET']
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_ACCESS_KEY'],
+      :region                 => ENV['AWS_REGION']
+    }
 
-  config.fog_directory = ENV['AWS_S3_BUCKET']
-
+    config.fog_directory = ENV['AWS_S3_BUCKET']
+  end
 end
