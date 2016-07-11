9e0c70cbedaa8910a36f7115d615156826287f8f
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-07-12 00:55:08 +0900

    closes #132 読んだよボタンを非同期にする

--- app/assets/javascripts/read_poem.js
+++ app/assets/javascripts/read_poem.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function () {
+  const interval = 500;
+  let timer;
+
+  $('.read_poem_btn').click(function(e){
+    const poem_id = $(e.target).data('poem-id');
+
+    $.ajax({
+      url: `/api/poems/${ poem_id }/read_poems`,
+      method: "post"
+    });
+
+    if (timer !== undefined) clearTimeout(timer);
+    
+    timer = setTimeout(function() {
+      location.reload();
+    }, interval);
+  });
+})
