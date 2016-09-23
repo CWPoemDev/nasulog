4fd2d9b75e88457d913e54b126a33336eda9e559
nasum <masaya.nasu@crowdworks.co.jp>
2016-09-23 18:52:57 +0900

    eslint導入

--- app/assets/javascripts/read_poem.js
+++ app/assets/javascripts/read_poem.js
@@ -1,21 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+/* global $ */
 $(function () {
   const interval = 500;
   let timer;
 
   $('.read_poem_btn').click(function(e){
-    const poem_id = $(e.target).data('poem-id');
+    const poemId = $(e.target).data('poem-id');
 
     $.ajax({
-      url: `/api/poems/${ poem_id }/read_poems`,
-      method: "post"
+      url: `/api/poems/${ poemId }/read_poems`,
+      method: 'post'
     });
 
     if (timer !== undefined) clearTimeout(timer);
-    
-    timer = setTimeout(function() {
+
+    timer = setTimeout(() => {
       location.reload();
     }, interval);
   });
-})
+});
